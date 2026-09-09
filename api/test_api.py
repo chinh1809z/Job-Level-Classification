f8c91421dffdf16757afa44c7981572d95b9f907
chinh1809z <trungchinhht36@gmail.com>
2026-09-09 21:35:52 +0700

    Add API test and requirements

---
+++ api/test_api.py
@@ -0,0 +1,20 @@
+import requests
+
+
+data = {
+    "title": "Senior Software Engineer",
+    "location": "Berlin",
+    "description": "We are looking for a senior software engineer to develop and maintain software applications.",
+    "function": "IT",
+    "industry": "Information Technology"
+}
+
+
+response = requests.post(
+    "http://127.0.0.1:5000/predict",
+    json=data
+)
+
+
+print(response.status_code)
+print(response.json())
